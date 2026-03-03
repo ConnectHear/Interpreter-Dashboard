26d990e40d31d46c72c16511fcbcef0553745547
Syed <mcareer259@gmail.com>
2026-03-03 11:09:13 +0500

    working on env issues regarding tz variale

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
+// MUST be the very first line — sets Node.js timezone before any Date objects are created
+process.env.TZ = 'Asia/Karachi';
+
 const app = require('../backend/app');
 
 module.exports = app;
